2fd5a4458454e91d50070d67aa09280fb8a7b014
w516656295 <516656295@qq.com>
2017-03-13 17:03:59 +0800

    前台

---
+++ moa_pre/src/main/java/premain/PreApplication.java
@@ -0,0 +1,10 @@
+package premain;  
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication  
+public class PreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PreApplication.class, args);
+	}
+}
